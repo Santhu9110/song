288ed42aacb30abf03e8979f2d6b0a844beda2c8
santhosh <c.santhoshmmc2003@gmail.com>
2024-04-01 06:58:13 +0000

    solution

---
+++ src/main/java/com/example/song/SongRepository.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public interface SongRepository{
+    ArrayList<Song> getAllSongs();
+    Song getSongById(int songId);
+    Song addSong(Song song);
+    Song updateSong(int songId,Song song);
+    void deleteSong(int songId);
+}
